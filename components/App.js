274b01987baef67c1ff99d0e1fe1991d3c4f4fd8
marek <kursygitarowekopernik@gmail.com>
2018-02-20 10:09:54 +0100

    usuniety zdublowany App

--- components/App.js
+++ components/App.js
@@ -36,13 +36,3 @@ var App = React.createClass({
     email: ''
     };
     
-    var App = React.createClass({
-    render: function() {
-        return (
-        React.createElement('div', {className: 'app'},
-            React.createElement(ContactForm, {contact: contactForm}),
-            React.createElement(Contacts, {items: contacts}, {})
-        )
-        );
-    }
-    });
